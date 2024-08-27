16a3f25c4a5128e9a947330d154d6090a281f92c
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-27 10:56:25 +0530

    Add: my cart all info completed

--- src/app/dashboard/mycart/mycart.component.ts
+++ src/app/dashboard/mycart/mycart.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mycart',
   templateUrl: './mycart.component.html',
-  styleUrls: ['./mycart.component.scss']
+  styleUrls: ['./mycart.component.scss'],
 })
 export class MycartComponent {
+  isOrderSummaryVisible = false;
+  isFormContainerVisible = false;
 
+  formFunction() { 
+    this.isFormContainerVisible = !this.isFormContainerVisible;
+  }
+
+  orderFunction() {
+    this.isOrderSummaryVisible = !this.isOrderSummaryVisible;
+  }
 }
