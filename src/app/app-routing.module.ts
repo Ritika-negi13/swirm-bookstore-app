315de48d23e90686d7971026ca103ef46c7a53b2
Sai Methish <Sai.Methish@godigit.com>
2024-08-28 12:12:41 +0530

    resolved conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 
 import { PleaseloginComponent } from './components/pleaselogin/pleaselogin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { MycartComponent } from './components/mycart/mycart.component';
 import { MyorderComponent } from './components/myorder/myorder.component';
 import { BookcardComponent } from './components/bookcard/bookcard.component';
