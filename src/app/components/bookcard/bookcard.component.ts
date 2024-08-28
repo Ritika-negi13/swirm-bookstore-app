0bb84e90162e02ea8ae98fd26dfe1554aac843a6
Washim Akram <Washim.Akram@godigit.com>
2024-08-28 17:46:13 +0530

    pagination added in bookcard

--- src/app/components/bookcard/bookcard.component.ts
+++ src/app/components/bookcard/bookcard.component.ts
@@ -9,32 +9,63 @@ import { BookdataService } from 'src/app/service/bookdata/bookdata.service';
 })
 export class BookcardComponent {
 
-  constructor(private https: HttpService, private bookdata : BookdataService) { }
+  constructor(private https: HttpService, private bookdata: BookdataService) { }
   books!: any[];
-  bookId : any;
+  bookId: any;
   bookImg: any[] = ["../../../assets/Image 8.png", "../../../assets/Image 7.png", "../../../assets/Image 11.png", "../../../assets/Image 12.png", "../../../assets/Image 36.png", "../../../assets/Image 8.png"];
-  filterBookList:any[]=[];
+  filterBookList: any[] = [];
   option: string = 'price';
-  
-  ngOnInit():void{
+
+  //pagination
+  currentPageno: number = 1;
+  itemsPerPage: number = 12;
+  totalPage: any;
+
+  ngOnInit(): void {
     this.bookdata.getBook();
     // this.books = this.bookdata.booksData;
-    this.bookdata.booksData.subscribe((book)=>{
+    this.bookdata.booksData.subscribe((book) => {
       console.log(book);
       this.books = book;
       this.filterBookList = this.books;
+      this.updatePagination();
     })
   }
-  filter(event:any){
-    this.option=event;
+  filter(event: any) {
+    this.option = event;
     this.filterBookList = this.books;
     this.filterBookList.sort((a: any, b: any) => {
       return a[event] - b[event];
     });
- }
- getId(e : any){
-  console.log(e);
-  this.bookdata.updateBookId(e);
- }
+    this.updatePagination();
+  }
+  getId(e: any) {
+    console.log(e);
+    this.bookdata.updateBookId(e);
+  }
+
+  //pagination
+  get paginationBook() {
+    const startIndex = (this.currentPageno - 1) * this.itemsPerPage;
+    return this.filterBookList.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  updatePagination() {
+    this.totalPage = Math.ceil(this.filterBookList.length / this.itemsPerPage);
+    this.currentPageno = 1;
+  }
+
+  //move to next page
+  nextPage() {
+    if (this.currentPageno < this.totalPage) {
+      this.currentPageno++;
+    }
+  }
+  //move to previous page
+  previousPage() {
+    if (this.currentPageno > 1) {
+      this.currentPageno--;
+    }
+  }
 
 }
