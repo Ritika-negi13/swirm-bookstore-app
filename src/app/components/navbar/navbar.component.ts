e3db99cecfc836cac2c46b0b816e193be6d645a6
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-29 22:29:47 +0530

    Add: add badge on cart icon

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,32 +2,48 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookService } from 'src/app/service/book/book.service';
 import { HttpService } from 'src/app/service/http/http.service';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { cart } from '../../../assets/cart'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
-  constructor(private router: Router, private http:HttpService, private book:BookService) { }
-  isLoggedIn=false;
+  isLoggedIn = false;
   showdialog = false;
-  ngOnInit(){
+  cartNo = 0;
+  constructor(
+    private router: Router,
+    private http: HttpService,
+    private book: BookService,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIconLiteral(
+      'cart',
+      sanitizer.bypassSecurityTrustHtml(cart)
+    );
+  }
+  ngOnInit() {
     /*******************************************************************************/
     //please do not remove this code, this is for admin login
-    this.http.adminlogin('/admin/login', {
-      email: 'admin@test.com',
-      password: 'test@123',
-    }).subscribe({
-      next: (res: any) => {
-        console.log("token:",res);
-        localStorage.setItem('admin_accesstoken', res.result.accessToken);
-      },
-      error: (error: any) => {
-        console.log(error);
-      }
-    });
+    this.http
+      .adminlogin('/admin/login', {
+        email: 'admin@test.com',
+        password: 'test@123',
+      })
+      .subscribe({
+        next: (res: any) => {
+          console.log('token:', res);
+          localStorage.setItem('admin_accesstoken', res.result.accessToken);
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+      });
 
     /*******************************************************************************/
     /*******************************************************************************/
@@ -35,6 +51,7 @@ export class NavbarComponent {
     this.book.getCartBooks('/get_cart_items').subscribe({
       next: (data: any) => {
         console.log('user', data.result[0].user_id._id);
+        this.cartNo = data.result.length;
         localStorage.setItem('userId', data.result[0].user_id._id);
       },
       error: (error) => {
@@ -50,23 +67,23 @@ export class NavbarComponent {
   }
   openwishlist() {
     this.router.navigate(['/wishlist']);
-    this.showdialog=false;
+    this.showdialog = false;
   }
   openorders() {
     this.router.navigate(['/myorder']);
-    this.showdialog=false;
+    this.showdialog = false;
   }
-  
+
   toggle() {
     this.showdialog = !this.showdialog;
   }
   openProfile = () => {
     this.router.navigate(['/profile']);
     this.showdialog = false;
-  }
+  };
 
-  logout=()=>{
-    localStorage.removeItem("acesstoken");
+  logout = () => {
+    localStorage.removeItem('acesstoken');
     window.location.reload();
-  }
+  };
 }
