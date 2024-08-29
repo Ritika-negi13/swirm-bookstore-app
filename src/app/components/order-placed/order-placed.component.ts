ec71a25b7279911378e4c00d77802fcd5b7ee2d8
sushma07662 <sushma.panduri@godigit.com>
2024-08-29 15:14:35 +0530

    made changes in order_placed page

--- src/app/components/order-placed/order-placed.component.ts
+++ src/app/components/order-placed/order-placed.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-order-placed',
   templateUrl: './order-placed.component.html',
   styleUrls: ['./order-placed.component.scss']
 })
 export class OrderPlacedComponent {
+  constructor(private router: Router){}
+  onBtnClick(){
+    this.router.navigate(['/bookcard'])
+  }
 
 }
